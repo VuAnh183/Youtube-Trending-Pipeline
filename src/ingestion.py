3abe9756f0d669e73e8ecabe4d782fcb6d4607f2
VuAnh183 <dinhvuanh2708@gmail.com>
2025-10-30 15:41:08 +0100

    Refactor: Use .env data instead of config.py

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,21 +1,33 @@
-import string
+import os
 import requests
 import json
+from dotenv import load_dotenv
+from pathlib import Path
 from googleapiclient.discovery import build
-from config import API_KEY, REGIONS
 
-def fetch_trending_videos(region_code: string, maxResults: int):
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+REGION = os.getenv("REGIONS")
+RAW_DIR = Path(os.getenv("RAW_DATA_DIR")) 
+
+file_path = RAW_DIR / "test.json"
+
+
+def fetch_trending_videos(maxResults: int):
     url = (
         f"https://www.googleapis.com/youtube/v3/videos?"
-        f"part=snippet,statistics&chart=mostPopular&regionCode={region_code}&"
+        f"part=snippet,statistics&chart=mostPopular&regionCode={REGION}&"
         f"maxResults={maxResults}&key={API_KEY}"
     )
 
     response =requests.get(url)
 
     data = response.json()
+    
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    print(json.dumps(data,indent = 2))
-
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    
 if __name__ == "__main__":
-    fetch_trending_videos("FR", 3)
+    fetch_trending_videos(10)
